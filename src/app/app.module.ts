d4c85a587bb842653322d8333268cc7c72fdd22f
Flo B Jones <flocela@gmail.com>
2018-05-30 16:13:59 -0500

    header for large view done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {DWIComponent} from './dwi/dwi.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'dwi', component: DWIComponent, pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DWIComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
+    RouterModule.forRoot((
+      appRoutes
+    )),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/dwi/dwi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dwi',
+  templateUrl: './dwi.component.html',
+  styleUrls: ['./dwi.component.scss']
+})
+export class DWIComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
