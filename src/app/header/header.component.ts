c197815d40849383de1e4779213ab12f3d035c58
Flo B Jones <flocela@gmail.com>
2018-05-31 07:24:37 -0500

    dropdown menu for different law areas works in header.component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  showingMenu = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showMenu() {
+    this.showingMenu = !this.showingMenu;
   }
 
 }
