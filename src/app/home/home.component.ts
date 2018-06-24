d8051a5218b3a41a1cf2b4fe2e1b6622e0ce3491
Flo B Jones <flocela@gmail.com>
2018-06-24 11:30:23 -0500

    finished with header placements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  width: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.width = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.width = window.innerWidth;
   }
 
 }
