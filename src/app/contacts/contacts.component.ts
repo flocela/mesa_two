0a7f30d22f38d2367746d9c75032f1e47289b55f
Flo B Jones <flocela@gmail.com>
2018-06-27 15:49:11 -0500

    contact page done. link to google maps added

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -17,9 +17,14 @@ export class ContactsComponent implements OnInit {
   sanAntonioGarageUrl = "url('https://firebasestorage.googleapis.com/v0/b/" +
     "maldonadoattorney-28622.appspot.com/o/san-antonio-garage.jpg?" +
     "alt=media&token=8b24c584-3825-4e91-920b-5c37b4072430')";
-
+  sanAntonioMapUrl = "url('https://firebasestorage.googleapis.com/v0/b/" +
+    "maldonadoattorney-28622.appspot.com/o/san-antonio-map.jpg?" +
+    "alt=media&token=89ee8ebe-fb65-4c4d-95fc-66844f4305d3')";
   directionsEnglish = "The entrance to the parking garage is on the 9th street side of our building.\n" +
     "Please ask the receptionist for a reimbursement ticket.";
+  googleClickUrl = "https://www.google.com/maps/place/The+Law+Office+of+Ricardo+Maldonado/@30.2719906,-97.7490377,17z/data=!3m1!4b1!4m5!3m4!1s0x8644b50b8895f4bb:0x83596371341f9913!8m2!3d30.271986!4d-97.746849";
+
+  googleMapsClickEnglish = "Go to google maps!";
   titleEnglish      = "Contact and Directions";
   street            = "812 San Antonio St. Suite 118";
   city              = "Austin, TX 78701";
@@ -33,9 +38,11 @@ export class ContactsComponent implements OnInit {
   fax: string;
   facebookfind: string;
   directions: string;
+  googleMapsClick: string;
   constructor() { }
 
   ngOnInit() {
+    this.googleMapsClick = this.googleMapsClickEnglish;
     this.title = this.titleEnglish;
     this.telephone = this.telephoneEnglish;
     this.fax = this.faxEnglish;
